5173ffb4384024ac8ad24530651069dbe3f9f853
Facundo Rouille <frouille@cibic.com.ar>
2025-12-11 12:54:52 -0300

    Configuracion adicional

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,18 @@
 import { useForm } from "react-hook-form";
 import { api } from "../api/api";
 import { useAuth } from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const { register, handleSubmit } = useForm();
   const { login } = useAuth();
+  const navigate = useNavigate();
 
   const onSubmit = async (data) => {
     try {
       const res = await api.post("/auth/login", data);
       login(res.data.token);
-      window.location.href = "/dashboard";
+      navigate("/dashboard");
     } catch (error) {
       alert("Credenciales inválidas");
     }
@@ -25,21 +27,13 @@ export default function Login() {
         <div style={{ marginBottom: "15px" }}>
           <label>Email:</label>
           <br />
-          <input
-            type="email"
-            {...register("email")}
-            required
-          />
+          <input type="email" {...register("email")} required />
         </div>
 
         <div style={{ marginBottom: "15px" }}>
           <label>Contraseña:</label>
           <br />
-          <input
-            type="password"
-            {...register("password")}
-            required
-          />
+          <input type="password" {...register("password")} required />
         </div>
 
         <button type="submit">Ingresar</button>
